344de1e5da3849615f9fe99272533d6b22d417a9
Harshal Ingle <harshalingle11092001@gmail.com>
2025-02-12 14:53:55 +0530

    proces.env for port

--- Backend/server.js
+++ Backend/server.js
@@ -11,7 +11,7 @@ import accountRouter from "./routes/accountRoute.js";
 
 // app config
 const app = express();
-const port = 4000;
+const port = process.env.PORT || 4000;
 
 // middleware
 app.use(express.json());
